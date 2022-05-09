afc2acc1a281c5a428f906b9e2c59487b91d3e5b
Ryan Oh <ryanohnoes@gmail.com>
2022-05-08 23:29:20 -0700

    fix aceLogic function to update b4 and after

--- blackjack/js/main.js
+++ blackjack/js/main.js
@@ -16,19 +16,21 @@ class BlackJack{
       fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(res=>res.json())
       .then(data=>{
-        console.log(data)
-        console.log(data.remaining)
         this.id = data.deck_id
         this.cardsInDeck = data.remaining;
-        this.draw()
+        this.deal()
       })
       .catch(err=>{
         console.log(`error ${err}`)
       })
     }
     else{
-      this.draw()
+      this.deal()
     }
+  }
+  handUpdate(){
+    this.playerTotal = this.playerCardsValue.reduce((sum,x) => sum + x,0)
+    this.dealerTotal = this.dealerCardsValue.reduce((sum,x) => sum +x,0)
   }
   convert(card){
     if (card === 'KING') return 10
@@ -55,21 +57,20 @@ class BlackJack{
     }
   }
   aceLogic(){
-    this.playerTotal = this.playerCardsValue.reduce((sum,x) => sum +x,0)
-    this.dealerTotal = this.dealerCardsValue.reduce((sum,x) => sum +x,0)
+    this.handUpdate()
     if(this.playerCardsValue.includes(11) && this.playerTotal > 21){
       this.playerCardsValue.splice(this.playerCardsValue.indexOf(11),1,1)
     }
     if(this.dealerCardsValue.includes(11) && this.dealerTotal > 21){
       this.dealerCardsValue.splice(this.dealerCardsValue.indexOf(11),1,1)
     }
+    this.handUpdate()
   }
   newDeck(){
     if(this.cardsInDeck < 15){
       fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
           .then(res=>res.json())
           .then(data=>{
-            console.log(data)
             this.id = (data.deck_id);
             this.cardsInDeck = data.remaining;
           })
@@ -78,7 +79,7 @@ class BlackJack{
           })
     }
   }
-  draw(){
+  deal(){
     this.reset();
     this.newDeck();
     if(this.playerCardsValue.length > 0) return
@@ -86,7 +87,6 @@ class BlackJack{
     fetch(`https://deckofcardsapi.com/api/deck/${this.id}/draw/?count=4`)
       .then(res=>res.json())
       .then(data=>{
-        console.log(data)
         for(let number in data.cards){
           
           if(number %2 === 0){
@@ -104,9 +104,8 @@ class BlackJack{
           }
         }
         this.cardsInDeck = data.remaining;
-        this.playerTotal = this.playerCardsValue.reduce((sum,x) => sum +x,0)
-        this.dealerTotal = this.dealerCardsValue.reduce((sum,x) => sum +x,0)
-        if(this.playerTotal=== 21) document.querySelector('h1').textContent = 'BLACKJACK'
+        this.handUpdate()
+        if(this.playerTotal=== 21) document.querySelector('h1').textContent = 'BLACKJACK!!!'
         if(this.dealerTotal=== 21) document.querySelector('h1').textContent = 'YOU LOSE'
         if(this.playerTotal ===21 && this.dealerTotal === 21) document.querySelector('h1').textContent = 'That sucks lol'
       })
@@ -117,10 +116,10 @@ class BlackJack{
 
   hit(){
     if(this.playerCardsValue.length === 0) return
+    if(document.querySelector('h1').textContent != '') return
     fetch(`https://deckofcardsapi.com/api/deck/${this.id}/draw/?count=1`)
     .then(res=>res.json())
     .then(data=>{
-      console.log(data)
       for(let number in data.cards){
           this.playerCardsImage.push(data.cards[number].image)
           this.playerCardsValue.push(this.convert(data.cards[number].value))
@@ -138,18 +137,14 @@ class BlackJack{
 
   playerBust(){
     this.aceLogic();
-    this.playerTotal = this.playerCardsValue.reduce((sum,x) => sum +x,0)
     if(this.playerTotal > 21) {
       return document.querySelector('h1').textContent = 'YOU LOSE'
-    }
-    
-    if(this.playerTotal === 21) {
+    } else if(this.playerTotal === 21) {
       return document.querySelector('h1').textContent = 'YOU WIN'
     }
   }
   dealerBust(){
     this.aceLogic();
-    this.dealerTotal = this.dealerCardsValue.reduce((sum,x) => sum +x,0)
     if(this.dealerTotal > 21) {
       document.querySelector('h1').textContent = 'YOU WIN'
       return
@@ -158,45 +153,47 @@ class BlackJack{
       document.querySelector('h1').textContent = 'YOU LOSE'
       return
     }
-    if (this.dealerTotal === this.playerTotal) {
-      document.querySelector('h1').textContent = 'PUSH'
-      return
-    }
     if(this.dealerTotal >= 17){
       if(this.playerTotal > this.dealerTotal){
         document.querySelector('h1').textContent = 'YOU WIN'
         return
+      }else if(this.dealerTotal === this.playerTotal){
+        document.querySelector('h1').textContent = 'PUSH'
+        return
       }else {
         document.querySelector('h1').textContent = 'YOU LOSE'
         return
-    } 
-   }
+      } 
+    }else{
+      this.end()
+    }
   }
   end(){
     if(this.playerCardsValue.length === 0) return
+    if(document.querySelector('h1').textContent != '') return
     if (this.dealerTotal >= 17){
       this.dealerBust()
       return
     }
+      fetch(`https://deckofcardsapi.com/api/deck/${this.id}/draw/?count=1`)
+      .then(res=>res.json())
+      .then(data=>{
+        for(let number in data.cards){
+          this.dealerCardsImage.push(data.cards[number].image)
+          this.dealerCardsValue.push(this.convert(data.cards[number].value))
+          const dealer= document.getElementById('dealer')
+          const img = document.createElement('img')
+          img.src = data.cards[number].image
+          dealer.appendChild(img)
+        }
+        this.cardsInDeck = data.remaining;
+        this.dealerBust();
+      })
+      .catch(err=>{
+        console.log(`error ${err}`)
+      })
     
-    fetch(`https://deckofcardsapi.com/api/deck/${this.id}/draw/?count=1`)
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data)
-      for(let number in data.cards){
-        this.dealerCardsImage.push(data.cards[number].image)
-        this.dealerCardsValue.push(this.convert(data.cards[number].value))
-        const dealer= document.getElementById('dealer')
-        const img = document.createElement('img')
-        img.src = data.cards[number].image
-        dealer.appendChild(img)
-      }
-      this.cardsInDeck = data.remaining;
-      this.dealerBust();
-    })
-    .catch(err=>{
-      console.log(`error ${err}`)
-    })
+
   }
 }
 
